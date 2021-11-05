7d8b8878bca7f6fedf7b2eb68d752cc506b5a6f3
ford <fordwoof9@outlook.com>
2021-11-05 14:12:39 +0000

    Replace files

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='./')
 
 @app.route('/')
-def home():
-    return render_template('Home.html')
+@app.route('/home')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('About.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('Contact.html')
+
+@app.route('/regulation')
+def regulation_page():
+    return render_template('Regulation.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
