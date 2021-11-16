e88b8b29a463e62fdbceb840ea74282e039be1d9
fordwoof9 <fordwoof9@outlook.com>
2021-11-16 23:33:14 +0700

    Get data from Main page

--- components/form/Index.js
+++ components/form/Index.js
@@ -10,6 +10,8 @@ import Swal from "sweetalert2";
 
 const Container = styled.div`
     display: flex;
+    padding-top: 5rem;
+    padding-left: 10rem;
     justify-content: center;
     .ant-form-item-control-input-content{
         text-align: center !important;
@@ -18,13 +20,31 @@ const Container = styled.div`
 
 export default function Index(props) {
     const [form] = Form.useForm()
-
     const onFinish = (values) => {
         console.log(values)
+        const axios = require('axios')
+        var formData = ({
+            user_name: values.stuname, 
+            user_email: values.email,
+            user_room: values.room
+        });
+        console.log(formData)
+        axios.post('http://localhost:5000/api/booking/create-booking', {JSON: formData}, {
+            headers: {
+                'content-type': 'application/json',
+            },
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
         {values ? Swal.fire('Good job!',
         'Reservation Successfully',
         'success') : <> </>}
     };
+
 
     return (
         <Container>
@@ -34,7 +54,7 @@ export default function Index(props) {
             >
                 <Form.Item name={['studentId']} className="form-item-student-id" rules={[{ required: true}]}>
                     <Input
-                        placeholder="Student ID"
+                        placeholder="รหัสนักศึกษา"
                         autoComplete="off"
                         onChange={(e) => {
                             let value = e.target.value.replace(/[^0-9]+/g, '')
@@ -42,25 +62,38 @@ export default function Index(props) {
                         }}
                     />
                 </Form.Item>
-                <Form.Item name={['name']} className="form-item-name" rules={[{ required: true}]}>
-                    <Input placeholder="Name"></Input>
+
+                <Form.Item name={['stuname']} className="form-item-name" rules={[{ required: true}]}>
+                    <Input placeholder="ชื่อนักศึกษา"></Input>
                 </Form.Item>
+
                 <Form.Item name={['email']} className="form-item-email" rules={[{ required: true}]}>
-                    <Input placeholder="Email"></Input>
+                    <Input placeholder="อีเมลของนักศึกษา"></Input>
                 </Form.Item>
+
                 <Form.Item name={['room']} className="form-item-room" rules={[{ required: true}]}>
-                    <Select placeholder="Room">
+                    <Select placeholder="กรุณาเลือกห้องประชุม">
                         <Select.Option value="r01">Room 1</Select.Option>
                         <Select.Option value="r02">Room 2</Select.Option>
                         <Select.Option value="r03">Room 3</Select.Option>
                         <Select.Option value="r04">Room 4</Select.Option>
+                        <Select.Option value="r05">Room 5</Select.Option>
+                        <Select.Option value="r06">Room 6</Select.Option>
+                        <Select.Option value="r07">Room 7</Select.Option>
+                        <Select.Option value="r08">Room 8</Select.Option>
+                        <Select.Option value="r09">Room 9</Select.Option>
+                        <Select.Option value="r10">Room 10</Select.Option>
+                        <Select.Option value="r11">Room 11</Select.Option>
+                        <Select.Option value="r12">Room 12</Select.Option>
                     </Select>
                 </Form.Item>
+
                 <Form.Item>
                     <Button type="primary" htmlType="submit">
-                        Submit
+                        ยืนยัน
                     </Button>
-                </Form.Item>                
+                </Form.Item> 
+
             </Form>
         </Container>
     )
