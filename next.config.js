f38923f61419fcec53d880f7d632333bc28a025b
fordwoof9 <fordwoof9@outlook.com>
2021-11-19 00:49:04 +0700

    Build Test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-    }
-  }
-
-  return {
-    /* config options for all phases except development here */
-  }
+/** 
+module.exports = {
+  reactStrictMode: true,
 }
+*/
+
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  /* config options here */
+}
+
+module.exports = nextConfig
--- pages/api/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "fetch_api");
-
-$studentId = $_POST['studentId'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$room = $_POST['room'];
-
-$sql = mysqli_query($conn, "INSERT INTO user (studentId, name, email, room) VALUES ('$studentId', '$name', '$email', '$room')");
